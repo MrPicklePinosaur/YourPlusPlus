908b1a4e05bd1ff9532832d03a0397328485cddc
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-03-29 13:59:46 -0400

    main window

---
+++ main.js
@@ -0,0 +1,18 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow} = electron;
+
+let mainWindow;
+
+app.on('ready',function(){
+	//create a new window
+	mainWindow = new BrowserWindow({});
+	//load html into window
+	mainWindow.loadURL(url.format({ //looks like file://dirname//mainWindow.html
+		pathname: path.join(__dirname, 'mainWindow.html'),
+		protocol: 'file:',
+		slashes: true
+	}));
+});
